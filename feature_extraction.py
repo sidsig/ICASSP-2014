fdabb8df7fab68656fc354e599624d67f2c9e580
Siddharth Sigtia <siddharth.sigtia@gmail.com>
2014-02-17 19:14:32 +0000

    Adding feat extraction to test on laptop.

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,35 +1,91 @@
 import numpy
 import subprocess
 import sys
+import os
 from spectrogram import SpecGram
+import tables
 import pdb
 
 def read_wav(filename):
-	bits_per_sample = '16'
-	cmd = ['sox',file_list[0],'-t','raw','-e','unsigned-integer','-L','-c','1','-b',bits_per_sample,'-','pad','0','30.0','rate','22050.0','trim','0','30.0']
-	cmd = ' '.join(cmd)
-	raw_audio = numpy.fromstring(subprocess.Popen(cmd,stdout=subprocess.PIPE,shell=True).communicate()[0],dtype='uint16')
-	max_amp = 2.**(int(bits_per_sample)-1)
-	raw_audio = (raw_audio- max_amp)/max_amp
-	return raw_audio
+    bits_per_sample = '16'
+    cmd = ['sox',filename,'-t','raw','-e','unsigned-integer','-L','-c','1','-b',bits_per_sample,'-','pad','0','30.0','rate','22050.0','trim','0','30.0']
+    cmd = ' '.join(cmd)
+    print cmd
+    raw_audio = numpy.fromstring(subprocess.Popen(cmd,stdout=subprocess.PIPE,shell=True).communicate()[0],dtype='uint16')
+    max_amp = 2.**(int(bits_per_sample)-1)
+    raw_audio = (raw_audio- max_amp)/max_amp
+    return raw_audio
 
 def calc_specgram(x,fs,winSize,):
-	spec = Specgram(x,fs,winSize)
-	return spec.specMat
+    spec = SpecGram(x,fs,winSize)
+    return spec.specMat
 
 
 def make_4tensor(x):
-	assert x.ndim <= 4
-	while x.ndim < 4:
-		x = numpy.expand_dims(x,0)
+    assert x.ndim <= 4
+    while x.ndim < 4:
+        x = numpy.expand_dims(x,0)
+    return x
+
+class FeatExtraction():
+    def __init__(self,dataset_dir):
+    	self.dataset_dir = dataset_dir
+        self.list_dir = os.path.join(self.dataset_dir,'lists')
+        self.get_filenames()
+        self.feat_dir = 
+        self.make_feat_dir()
+        self.h5_filename = os.path.join(self.feat_dir,'feats.h5')
+        self.make_h5()
+        self.setup_h5()
+        self.extract_features()
 
 
+    def get_filenames(self,):
+        dataset_files = os.path.join(self.list_dir,'audio_files.txt')
+        self.filenames = [l.strip() for l in open(dataset_files,'r').readlines()]
+        self.num_files = len(self.filenames)
 
-filename = '/home/siggy/PhD/gtzan/lists/audio_files.txt'
+    def make_feat_dir(self,):
+    	if not os.path.exists(self.feat_dir):
+    		print 'Making output dir.'
+    		os.mkdir(self.feat_dir)
+    	else:
+    		print 'Output dir already exists.'
+    
+    def make_h5(self,):
+    	if not os.path.exists(self.h5_filename):
+    		self.h5 = tables.openFile(self.h5_filename,'w')
+    	else:
+    		print 'Feature file already exists.'
+    		self.h5 = tables.openFile(self.h5_filename,'a')
 
-file_list = [l.strip() for l in open(filename,'r').readlines()]
+    def setup_h5(self,):
+    	filename = self.filenames[8]
+    	x = read_wav(filename)
+    	spec_x = calc_specgram(x,22050,1024)
+    	spec_x = make_4tensor(spec_x)
+    	self.data_shape = spec_x.shape[1:]
+    	self.x_earray_shape = (0,) + self.data_shape
+    	self.chunkshape = (1,) + self.data_shape
+    	self.h5_x = self.h5.createEArray('/','x',tables.FloatAtom(itemsize=4),self.x_earray_shape,chunkshape=self.chunkshape,expectedrows=self.num_files)
+    	self.h5_filenames = self.h5.createEArray('/','filenames',tables.StringAtom(256),(0,),expectedrows=self.num_files)
+    	self.h5_x.append(spec_x)
+    	self.h5_filenames.append([filename])
 
 
-data = read_wav(file_list[0])
-test = SpecGram(data,22050,1024)
-pdb.set_trace()
+    def extract_features(self,):
+    	for i in xrange(1,self.num_files):
+    		filename = self.filenames[i]
+    		x = read_wav(filename)
+    		spec_x = calc_specgram(x,22050,1024)
+    		spec_x = make_4tensor(spec_x)
+    		self.h5_x.append(spec_x)
+    		self.h5_filenames.append([filename])
+
+if __name__ == '__main__':
+	test = FeatExtraction('/homes/sss31/datasets/gtzan/')
+    # filename = '/home/siggy/PhD/gtzan/lists/audio_files.txt'
+    # file_list = [l.strip() for l in open(filename,'r').readlines()]
+    # data = read_wav(file_list[0])
+    # test = SpecGram(data,22050,1024)
+    # pdb.set_trace()
