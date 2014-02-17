57994000c54068053d3fe6c22ddfa9d0879942e2
sidsig <siddharth.sigtia@gmail.com>
2014-02-17 16:54:49 +0000

    Finishing dataset code.

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,6 +1,7 @@
 import numpy
 import subprocess
 import sys
+from spectrogram import SpecGram
 import pdb
 
 def read_wav(filename):
@@ -9,13 +10,26 @@ def read_wav(filename):
 	cmd = ' '.join(cmd)
 	raw_audio = numpy.fromstring(subprocess.Popen(cmd,stdout=subprocess.PIPE,shell=True).communicate()[0],dtype='uint16')
 	max_amp = 2.**(int(bits_per_sample)-1)
-	pdb.set_trace()
 	raw_audio = (raw_audio- max_amp)/max_amp
 	return raw_audio
+
+def calc_specgram(x,fs,winSize,):
+	spec = Specgram(x,fs,winSize)
+	return spec.specMat
+
+
+def make_4tensor(x):
+	assert x.ndim <= 4
+	while x.ndim < 4:
+		x = numpy.expand_dims(x,0)
+
+
 
 filename = '/home/siggy/PhD/gtzan/lists/audio_files.txt'
 
 file_list = [l.strip() for l in open(filename,'r').readlines()]
 
-data = read_wav(file_list[0])
 
+data = read_wav(file_list[0])
+test = SpecGram(data,22050,1024)
+pdb.set_trace()
